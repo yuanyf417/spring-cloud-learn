419e5ff4d899687707b9fbfeb915841a76ef8cff
YF Yuan <yuanyf417@163.com>
2024-10-04 21:19:41 +0800

    feat: 配置中心nacos示例

---
+++ learn-nacos-config/src/test/java/com/yyf/learnnacosconfig/LearnNacosConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyf.learnnacosconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnNacosConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
