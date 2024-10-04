dd9e5dd08b71ab8dd0554b039d3de75a16a4e1e5
YF Yuan <yuanyf417@163.com>
2024-10-04 19:34:43 +0800

    feat: 服务注册nacos示例

---
+++ learn-nacos-discovery/src/main/java/com/yyf/learnnacosdiscovery/LearnNacosDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.yyf.learnnacosdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LearnNacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnNacosDiscoveryApplication.class, args);
+    }
+
+}
